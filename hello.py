94af1af64b4b5cd96dbac32f5642e40aed38e79d
University Chatbot <user@example.com>
2026-06-09 19:40:38 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,37 @@
+print("Hello World")
+print("Welcome to AI and Machine Learning.")
+
+name = "Mahek"
+age = 22
+height = 5.2
+
+print(name)
+print(age)
+print(height)
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+lang = "Python"
+
+#Exercise 1
+print(name)
+print(age)
+print(lang)
+
+#Exercise 2
+bookname = "The Alchemist"
+price = 300
+pages = 500
+isavailable = True
+
+print(type(bookname))
+print(type(price))
+print(type(pages))
+print(type(isavailable))
+
+#Exercise 3
+print("My name is Mahek.")
+print("I want to become an AI Engineer.")
+print("I am starting my Python journey today.")
---
+++ my_profile.py
@@ -0,0 +1,15 @@
+name = "Mahek"
+age = "22"
+city = "Mumbai"
+country = "India"
+college = "XYZ College"
+dreamJob = "AI Engineer"
+
+print("------ MY PROFILE ------")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
+print(f"Country: {country}")
+print(f"College: {college}")
+print(f"Dream Job: {dreamJob}")
+print("------------------------")
