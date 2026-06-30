2ade2fe311a2d7bf8c4ed5e047ebc8fa33c09bf7
University Chatbot <user@example.com>
2026-06-30 18:31:13 +0530

    feat: implement string analysis exercises and add a new palindrome checker script

--- month-1/day-5/strings.py
+++ month-1/day-5/strings.py
@@ -3,3 +3,21 @@ name = input("Enter name: ")
 print(f"Original: {name}")
 print(f"Uppercase: {name.upper()}")
 print(f"Lowercase: {name.lower()}")
+
+sent = input("Enter sentence: ")
+print(f"Total characters: {len(sent)}")
+print(f"Number of space: {sent.count(' ')}")
+
+word = input("Enter a word: ")
+print(f"First character: {word[0]}")
+print(f"Last character: {word[-1]}")
+
+#Take a sentence.
+#Replace:
+#AI
+#with
+#Artificial Intelligence
+
+sent = input("Enter sentence: ")
+sent = sent.replace("AI", "Artificial Intelligence")
+print(sent)
